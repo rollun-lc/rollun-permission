304061968ae8e082e3eda219ee944f7136878f20
victorynox <it.professor02@gmail.com>
2017-02-17 19:48:38 +0200

    fix acl

--- src/Auth/Middleware/ErrorHandler/AccessForbiddenHandlerMiddleware.php
+++ src/Auth/Middleware/ErrorHandler/AccessForbiddenHandlerMiddleware.php
@@ -12,6 +12,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use rollun\permission\Acl\AccessForbiddenException;
 use rollun\permission\Acl\Middleware\RoleResolver;
+use rollun\permission\Auth\Middleware\UserResolver;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\RedirectResponse;
 use Zend\Expressive\Helper\UrlHelper;
@@ -28,8 +29,8 @@ class AccessForbiddenHandlerMiddleware
 
     public function __invoke($error, Request $request, Response $response, callable $next) {
         if ($error instanceof AccessForbiddenException) {
-            $roles = $request->getAttribute(RoleResolver::KEY_ROLE_ATTRIBUTE);
-            if (empty(array_diff($roles, [RoleResolver::DEFAULT_ROLE]))) {
+            $user = $request->getAttribute(UserResolver::KEY_USER);
+            if (empty(array_diff([RoleResolver::DEFAULT_ROLE], $user['roles']))) {
                 $url = $this->urlHelper->generate('login');
                 $response = new RedirectResponse($url, 302, ['Location' => filter_var($url, FILTER_SANITIZE_URL)]);
             } else {
