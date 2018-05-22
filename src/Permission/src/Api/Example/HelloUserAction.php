c93294bf6988f840f558fc941578b37cd26e10b6
victorynox <it.professor02@gmail.com>
2018-05-22 13:56:47 +0300

    Fixed example middleware

--- src/Permission/src/Api/Example/HelloUserAction.php
+++ src/Permission/src/Api/Example/HelloUserAction.php
@@ -8,44 +8,27 @@
 
 namespace rollun\permission\Api\Example;
 
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use rollun\permission\Auth\Middleware\UserResolver;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Expressive\Template\TemplateRendererInterface;
-use Zend\Stratigility\MiddlewareInterface;
 
 class HelloUserAction implements MiddlewareInterface
 {
-
     /**
-     * Process an incoming request and/or response.
-     *
-     * Accepts a server-side request and a response instance, and does
-     * something with them.
-     *
-     * If the response is not complete and/or further processing would not
-     * interfere with the work done in the middleware, or if the middleware
-     * wants to delegate to another process, it can use the `$out` callable
-     * if present.
-     *
-     * If the middleware does not return a value, execution of the current
-     * request is considered complete, and the response instance provided will
-     * be considered the response to return.
-     *
-     * Alternately, the middleware may return a response instance.
-     *
-     * Often, middleware will `return $out();`, with the assumption that a
-     * later middleware will return a response.
+     * Process an incoming server request and return a response, optionally delegating
+     * to the next middleware component to create the response.
      *
      * @param Request $request
-     * @param Response $response
-     * @param null|callable $out
-     * @return null|Response
-     * @throws \Exception
+     * @param DelegateInterface $delegate
+     *
+     * @return Response
      */
-    public function __invoke(Request $request, Response $response, callable $out = null)
+    public function process(Request $request, DelegateInterface $delegate)
     {
         $data = [];
         $name = $request->getAttribute('name');
@@ -57,10 +40,7 @@ class HelloUserAction implements MiddlewareInterface
             throw new \Exception("Exception by string: ".  $data['str']);
         }
         $request = $request->withAttribute('responseData', $data);
-
-        if (isset($out)) {
-            return $out($request, $response);
-        }
+        $response = $delegate->process($request);
         return $response;
     }
 }
