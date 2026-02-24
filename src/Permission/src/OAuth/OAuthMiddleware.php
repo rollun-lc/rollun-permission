af6548581a968ced54cfeecb507ff2e05aa2c4db
vadymk-rollun <kopylovvadym.rollun@gmail.com>
2026-02-24 17:45:01 +0000

    feat(Fx7uzVnJ): update OAuthMiddleware.php - validation host from request with hosts whitelist & test

--- src/Permission/src/OAuth/OAuthMiddleware.php
+++ src/Permission/src/OAuth/OAuthMiddleware.php
@@ -91,25 +91,65 @@ abstract class OAuthMiddleware implements MiddlewareInterface
 
     /**
      * @param $action
+     * @param ServerRequestInterface|null $request
      * @return string|null
      */
-    protected function actionToRedirectUri($action): ?string
+    protected function actionToRedirectUri($action, ServerRequestInterface $request = null): ?string
     {
         $routeNameConfig = $action . 'RouteName';
 
-        foreach ([$routeNameConfig, 'host'] as $config) {
-            if (!$this->getConfig($config)) {
-                throw new InvalidArgumentException("Missing '$config' config for redirect");
-            }
+        if (!$this->getConfig($routeNameConfig)) {
+            throw new InvalidArgumentException("Missing '$routeNameConfig' config for redirect");
         }
+
+        $host = $this->resolveHost($request);
+
         $this->logger->debug('actionToRedirectUri', [
             'config' => $this->config,
             'action' => $action,
             'routeNameConfig' => $routeNameConfig,
-            'host' => $this->getConfig('host'),
+            'host' => $host,
             'path' => $this->urlHelper->generate($this->getConfig($routeNameConfig))
         ]);
 
-        return $this->getConfig('host') . $this->urlHelper->generate($this->getConfig($routeNameConfig));
+        return $host . $this->urlHelper->generate($this->getConfig($routeNameConfig));
+    }
+
+    /**
+     * Resolve host from request, validated against whitelist from config.
+     * Config 'host' may contain comma-separated whitelist (e.g. "https://host1.com,https://host2.com").
+     * First entry is used as fallback.
+     *
+     * @param ServerRequestInterface|null $request
+     * @return string
+     */
+    private function resolveHost(ServerRequestInterface $request = null): string
+    {
+        $hostConfig = $this->getConfig('host');
+
+        if (!$hostConfig) {
+            throw new InvalidArgumentException("Missing 'host' config for redirect");
+        }
+
+        $whitelist = array_map('trim', explode(',', $hostConfig));
+        $fallback = $whitelist[0];
+
+        if (!$request) {
+            return $fallback;
+        }
+
+        $requestHost = $request->getUri()->getScheme() . '://' . $request->getUri()->getHost();
+
+        if (in_array($requestHost, $whitelist, true)) {
+            return $requestHost;
+        }
+
+        $this->logger->debug('resolveHost: request host not in whitelist, using fallback', [
+            'requestHost' => $requestHost,
+            'whitelist' => $whitelist,
+            'fallback' => $fallback,
+        ]);
+
+        return $fallback;
     }
 }
--- tests/functional/Permission/Authentication/OAuth/Middleware/OAuthRedirectMiddlewareTest.php
+++ tests/functional/Permission/Authentication/OAuth/Middleware/OAuthRedirectMiddlewareTest.php
@@ -11,6 +11,8 @@ use Mezzio\Helper\UrlHelper;
 use Mezzio\Session\SessionInterface;
 use Mezzio\Session\SessionMiddleware;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Log\LoggerInterface;
 use rollun\dic\InsideConstruct;
@@ -19,28 +21,24 @@ use rollun\permission\OAuth\RedirectMiddleware;
 
 class OAuthRedirectMiddlewareTest extends TestCase
 {
-    public function testProcess()
-    {
-        $container = require 'config/container.php';
-        InsideConstruct::setContainer($container);
+    private $clientId = 'client-id';
+    private $projectId = 'rollun-test';
+    private $approvalPrompt = 'auto';
+    private $state = 'someState';
+    private $scope = 'openid';
+    private $accessType = 'online';
 
-        $clientId = 'client-id';
-        $projectId = 'rollun-test';
-        $redirectUrl = 'http://localhost';
-        $approvalPrompt = 'auto';
-        $state = 'someState';
-        $scope = 'openid';
-        $accessType = 'online';
+    private function createRedirectMiddleware(string $host): RedirectMiddleware
+    {
         $googleClientConfig = [
-            'client_id'        => $clientId,
-            'project_id'       => $projectId,
-            'redirect_uri'     => $redirectUrl,
-            'access_type'      => $accessType,
-            'approval_prompt'  => $approvalPrompt,
-            'state'            => $state,
+            'client_id'        => $this->clientId,
+            'project_id'       => $this->projectId,
+            'redirect_uri'     => 'http://localhost',
+            'access_type'      => $this->accessType,
+            'approval_prompt'  => $this->approvalPrompt,
+            'state'            => $this->state,
         ];
 
-        // Используем mock для UrlHelper, чтобы не зависеть от роутера
         $urlHelper = $this->createMock(UrlHelper::class);
         $urlHelper->expects($this->any())
             ->method('generate')
@@ -51,55 +49,93 @@ class OAuthRedirectMiddlewareTest extends TestCase
 
         $googleClient = new GoogleClient($googleClientConfig);
         $config = [
-            'scopes'         => $scope,
-            'host'           => $redirectUrl,
+            'scopes'         => $this->scope,
+            'host'           => $host,
             'loginRouteName' => 'login',
         ];
-        $object = new RedirectMiddleware(
-            $googleClient,
-            $urlHelper,
-            $logger,
-            $config
-        );
 
-        // Создаем mock сессии и добавляем ее в запрос
+        return new RedirectMiddleware($googleClient, $urlHelper, $logger, $config);
+    }
+
+    private function createRequest(string $uri): ServerRequestInterface
+    {
         $session = $this->createMock(SessionInterface::class);
-        $request = (new ServerRequest())
+
+        return (new ServerRequest([], [], $uri))
             ->withQueryParams(['action' => 'login'])
             ->withAttribute(SessionMiddleware::SESSION_ATTRIBUTE, $session);
+    }
+
+    private function getRedirectUriFromResponse(ResponseInterface $response): string
+    {
+        $location = $response->getHeader('Location')[0];
+        $parsedUrl = parse_url($location);
+        parse_str($parsedUrl['query'], $queryParams);
+
+        return urldecode($queryParams['redirect_uri']);
+    }
+
+    public function testProcess()
+    {
+        $container = require 'config/container.php';
+        InsideConstruct::setContainer($container);
+
+        $redirectUrl = 'http://localhost';
+        $object = $this->createRedirectMiddleware($redirectUrl);
+        $request = $this->createRequest('http://localhost/oauth/redirect');
 
         /** @var RequestHandlerInterface $handler */
         $handler = $this->getMockBuilder(RequestHandlerInterface::class)->getMock();
 
-        // Выполняем middleware
         $response = $object->process($request, $handler);
 
-        // Проверяем, что ответ имеет статус 302 (Redirect)
         $this->assertEquals(302, $response->getStatusCode());
 
-        // Получаем URL редиректа
         $location = $response->getHeader('Location')[0];
 
-        // Разбираем URL на составные части
         $parsedUrl = parse_url($location);
         $this->assertEquals('https', $parsedUrl['scheme']);
         $this->assertEquals('accounts.google.com', $parsedUrl['host']);
-        // Ожидаемый путь от Google OAuth (в актуальной версии используется /o/oauth2/v2/auth)
         $this->assertEquals('/o/oauth2/v2/auth', $parsedUrl['path']);
 
-        // Разбираем query-параметры
         parse_str($parsedUrl['query'], $queryParams);
 
         $this->assertEquals('code', $queryParams['response_type']);
-        $this->assertEquals($accessType, $queryParams['access_type']);
-        $this->assertEquals($clientId, $queryParams['client_id']);
-        // Проверяем, что redirect_uri соответствует http://localhost/login
+        $this->assertEquals($this->accessType, $queryParams['access_type']);
+        $this->assertEquals($this->clientId, $queryParams['client_id']);
         $this->assertEquals($redirectUrl . '/login', urldecode($queryParams['redirect_uri']));
-        // Параметр state генерируется динамически – проверяем, что он не пустой
         $this->assertNotEmpty($queryParams['state']);
-        // Проверяем scope
-        $this->assertEquals($scope, $queryParams['scope']);
-        // Если параметр approval_prompt не добавляется, можно проверить его отсутствие
+        $this->assertEquals($this->scope, $queryParams['scope']);
         $this->assertArrayNotHasKey('approval_prompt', $queryParams);
+    }
+
+    public function testRedirectUriUsesRequestHostFromWhitelist()
+    {
+        $container = require 'config/container.php';
+        InsideConstruct::setContainer($container);
+
+        $object = $this->createRedirectMiddleware('http://primary.com,http://secondary.com');
+        $request = $this->createRequest('http://secondary.com/oauth/redirect');
+        $handler = $this->getMockBuilder(RequestHandlerInterface::class)->getMock();
+
+        $response = $object->process($request, $handler);
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('http://secondary.com/login', $this->getRedirectUriFromResponse($response));
+    }
+
+    public function testRedirectUriFallsBackToFirstHostWhenRequestHostNotInWhitelist()
+    {
+        $container = require 'config/container.php';
+        InsideConstruct::setContainer($container);
+
+        $object = $this->createRedirectMiddleware('http://primary.com,http://secondary.com');
+        $request = $this->createRequest('http://unknown.com/oauth/redirect');
+        $handler = $this->getMockBuilder(RequestHandlerInterface::class)->getMock();
+
+        $response = $object->process($request, $handler);
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('http://primary.com/login', $this->getRedirectUriFromResponse($response));
     }
 }
